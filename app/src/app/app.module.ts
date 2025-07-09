054f5386936e0d769a8a9d99c0ff2fea5268ceb1
Luca Pagliaro <pagliaroluca@gmail.com>
2025-07-09 21:05:27 +0200

    feat: implement login page

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { provideHttpClient, withFetch } from "@angular/common/http";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {provideHttpClient, withFetch} from "@angular/common/http";
 import {providePrimeNG} from "primeng/config";
 import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
 import Theme from './shared/theme';
 import {ButtonModule} from "primeng/button";
+import {FormsModule} from "@angular/forms";
+import {LoginPageComponent} from "./auth/pages/login-page/login-page.component";
 
 @NgModule({
   declarations: [
@@ -17,6 +19,8 @@ import {ButtonModule} from "primeng/button";
     BrowserModule,
     AppRoutingModule,
     ButtonModule,
+    FormsModule,
+    LoginPageComponent
   ],
   providers: [
     provideHttpClient(
@@ -31,4 +35,5 @@ import {ButtonModule} from "primeng/button";
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ app/src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,36 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {InputText} from "primeng/inputtext";
+import {Fieldset} from "primeng/fieldset";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Button} from "primeng/button";
+import {MessageModule} from "primeng/message";
+
+@Component({
+  selector: 'app-login-page',
+  imports: [
+    CommonModule,
+    InputText,
+    Fieldset,
+    ReactiveFormsModule,
+    Button,
+    MessageModule
+  ],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css'
+})
+export class LoginPageComponent {
+  formBuilder = new FormBuilder();
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]]
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    const formValue = this.loginForm.value;
+    console.log('credentials:', formValue);
+  }
+}
